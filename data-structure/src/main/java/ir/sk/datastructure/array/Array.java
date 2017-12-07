ee47d895913aa5169c59fb6bb9115c8293dd9e7d
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2017-12-07 16:20:38 +0330

    Array data structure added.

---
+++ data-structure/src/main/java/ir/sk/datastructure/array/Array.java
@@ -0,0 +1,54 @@
+package ir.sk.datastructure.array;
+
+/**
+ * Unordered array data structure like Set in java. Duplicate isn't allowed.
+ * When duplicate is allowed, It's like ArrayList in java
+ * @author <a href="kayvanfar.sj@gmail.com">Saeed Kayvanfar</a> on 12/7/2017.
+ */
+public class Array<T> {
+
+    private Object[] a;                     // ref to array a
+    private int num;                        // number of data items
+
+    public Array(int max) {
+        a = new Object[max];                // create the array
+        num = 0;                            // no items yet
+    }
+
+    public boolean find(T searchKey) {
+        int j;
+        for(j = 0; j < num; j++)            // for each element,
+            if(a[j] == searchKey)           // found item?
+                break;                      // exit loop before end
+        if(j == num)                        // gone to end?
+            return false;                   // yes, can't find it
+        else
+            return true;                    // no, found it
+    }
+
+    public void insert(T value) {           // put element into array
+        a[num] = value;                     // insert it
+        num++;                              // increment size
+    }
+
+    public boolean delete(T value) {
+        int j;
+        for(j = 0; j < num; j++)            // look for it
+            if(value == a[j])
+                break;
+        if(j == num)                        // can't find it
+            return false;
+        else {                              // found it
+            for(int k = j; k < num; k++)    // move higher ones down
+                a[k] = a[k+1];
+            num--;                          // decrement size
+            return true;
+        }
+    }
+
+    public void display() {                 // displays array contents
+        for(int j=0; j<num; j++)            // for each element,
+            System.out.print(a[j] + " ");   // display it
+        System.out.println("");
+    }
+}
---
+++ data-structure/src/test/java/ir/sk/datastructure/array/ArrayTest.java
@@ -0,0 +1,63 @@
+package ir.sk.datastructure.array;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author <a href="kayvanfar.sj@gmail.com">Saeed Kayvanfar</a> on 12/7/2017.
+ */
+public class ArrayTest {
+
+    Array<Integer> array;
+
+    @Before
+    public void setUp() throws Exception {
+        int maxSize = 100;
+        array = new Array<>(maxSize);
+
+        // insert 10 items
+        array.insert(77);
+        array.insert(99);
+        array.insert(44);
+        array.insert(55);
+        array.insert(22);
+        array.insert(88);
+        array.insert(11);
+        array.insert(00);
+        array.insert(66);
+        array.insert(33);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void find() throws Exception {
+        int searchKey = 35; // search for item
+        if(array.find(searchKey))
+            System.out.println("Found " + searchKey);
+        else
+            System.out.println("Can’t find " + searchKey);
+    }
+
+    @Test
+    public void insert() throws Exception {
+        array.display();
+    }
+
+    @Test
+    public void delete() throws Exception {
+        array.delete(00); // delete 3 items
+        array.display();
+    }
+
+    @Test
+    public void display() throws Exception {
+        array.display();
+    }
+
+}
