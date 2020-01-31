01a918d586da997e0dd33d4b35cfe846f71114cb
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-01-31 13:41:06 +0330

    Simple array-based priority queue is added.

---
+++ data-structure/src/main/java/ir/sk/datastructure/queue/PriorityQueue.java
@@ -0,0 +1,75 @@
+package ir.sk.datastructure.queue;
+
+import java.util.Arrays;
+
+/**
+ * Simple array-based priority queue
+ * This implementation suffers from slow insertion, but it’s
+ * simpler and is appropriate when the number of items isn’t high or insertion speed
+ * isn’t critical.
+ *
+ * @author <a href="kayvanfar.sj@gmail.com">Saeed Kayvanfar</a> on 1/31/2020.
+ */
+public class PriorityQueue<T extends Comparable> {
+
+    // array in sorted order, from max at 0 to min at size-1
+    private int maxSize;
+    private Comparable[] queArray;
+    private int nItems;
+
+    public PriorityQueue(int s) {
+        maxSize = s;
+        queArray = new Comparable[maxSize];
+        nItems = 0;
+    }
+
+    public void insert(T item) // insert item
+    {
+        int j;
+        if (nItems == 0) // if no items,
+            queArray[nItems++] = item; // insert at 0
+        else // if items,
+        {
+            for (j = nItems - 1; j >= 0; j--) // start at end,
+            {
+                if (item.compareTo(queArray[j]) > 0) // if new item larger,
+                    queArray[j + 1] = queArray[j]; // shift upward
+                else // if smaller,
+                    break; // done shifting
+            } // end for
+            queArray[j + 1] = item; // insert it
+            nItems++;
+        } // end else (nItems > 0)
+    } // end insert()
+
+    public T remove() // remove minimum item
+    {
+        return (T) queArray[--nItems];
+    }
+
+    public T peekMin() // peek at minimum item
+    {
+        return (T) queArray[nItems - 1];
+    }
+
+    public boolean isEmpty() // true if queue is empty
+    {
+        return (nItems == 0);
+    }
+
+    public boolean isFull() // true if queue is full
+    {
+        return (nItems == maxSize);
+    }
+
+    public void display() {
+        System.out.print("\nPriorityQueue = ");
+        if (queArray.length == 0) {
+            System.out.print("Empty\n");
+            return;
+        }
+        Arrays.stream(queArray).forEach(o -> System.out.print(", " + o));
+        System.out.println();
+    }
+
+}
---
+++ data-structure/src/test/java/ir/sk/datastructure/queue/PriorityQueueTest.java
@@ -0,0 +1,58 @@
+package ir.sk.datastructure.queue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author <a href="kayvanfar.sj@gmail.com">Saeed Kayvanfar</a> on 1/31/2020.
+ */
+public class PriorityQueueTest {
+
+    PriorityQueue<Integer> theQueue;
+
+    @Before
+    public void setUp() throws Exception {
+        theQueue = new PriorityQueue<>(10);
+        theQueue.insert(10); // insert 4 items
+        theQueue.insert(20);
+        theQueue.insert(30);
+        theQueue.insert(40);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void insert() {
+        theQueue.display();
+        theQueue.insert(50);
+        theQueue.display();
+    }
+
+    @Test
+    public void remove() {
+        theQueue.display();
+        theQueue.remove();
+        theQueue.display();
+    }
+
+    @Test
+    public void peekMin() {
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void isFull() {
+    }
+
+    @Test
+    public void display() {
+    }
+}
