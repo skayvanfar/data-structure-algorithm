fdf459fd2d54354c8c78f4796444df3bbb60d983
Sara Behroozi <vafayi.ali27@gmail.com>
2020-06-25 18:26:24 +0430

    binarySearch is improved.

--- data-structure/src/main/java/ir/sk/algorithm/Search.java
+++ data-structure/src/main/java/ir/sk/algorithm/Search.java
@@ -8,58 +8,69 @@ public class Search {
     /**
      * @param array
      * @param searchKey
-     * @param <T>
      * @return
      */
-    public static <T> int linerSearch(T array[], T searchKey) {
+    public static int linerSearch(int array[], int searchKey) {
         for (int i = 0; i < array.length; i++) {
-            if (array[i].equals(searchKey))
+            if (array[i] == searchKey)
                 return i;
         }
         return -1;
     }
 
     /**
+     * binary search, also known as half-interval search,[1] logarithmic search,[2] or binary chop,[3] is a search algorithm
+     * that finds the position of a target value within a sorted array.
+     * Binary search compares the target value to the middle element of the array.
+     * If they are not equal, the half in which the target cannot lie is eliminated and the search continues on the remaining half,
+     * again taking the middle element to compare to the target value, and repeating this until the target value is found.
+     * If the search ends with the remaining half being empty, the target is not in the array
+     *
+     * decrease-and-conquer (Divide-and-conquer algorithm) algorithm and iterative
+     * Time Complexity (Log n)
+     *
      * @param array
-     * @param searchKey
-     * @param <T>
+     * @param key
      * @return
      */
-    public static <T extends Comparable> int binarySearchByLoop(T array[], T searchKey) {
+    public static int binarySearchByLoop(int array[], int key) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (key == array[mid])
+                return mid;
+            else if (key < array[mid])
+                high = mid + 1;
+            else
+                low = mid + 1;
+        }
+        return 0;
+    }
 
-        int lowerBound = 0;
-        int upperBound = array.length - 1;
-        int curIn;
-        while(true)
-        {
-            curIn = (lowerBound + upperBound ) / 2;
-            if(array[curIn]==searchKey)
-                return curIn; // found it
-            else if(lowerBound > upperBound)
-                return array.length; // can’t find it
-            else // divide range
-            {
-                if(array[curIn].compareTo(searchKey) < 0)
-                    lowerBound = curIn + 1; // it’s in upper half
-                else
-                    upperBound = curIn - 1; // it’s in lower half
-            } // end else divide range
-        } // end while
-    } // end find()
-
-    public static <T extends Comparable> int binarySearchByRecursive(T array[], T searchKey, int lowerBound, int upperBound) {
-        int curIn;
-        curIn = (lowerBound + upperBound ) / 2;
-        if(array[curIn]==searchKey)
-            return curIn; // found it
-        else if(lowerBound > upperBound)
-            return array.length; // can’t find it
-        else // divide range
-        {
-            if(array[curIn].compareTo(searchKey) < 0) // it’s in upper half
-                return binarySearchByRecursive(array, searchKey, curIn+1, upperBound);
-            else // it’s in lower half
-                return binarySearchByRecursive(array, searchKey, lowerBound, curIn-1);
-        } // end else divide range
-    } // end recFind()
+    /**
+     * decrease-and-conquer (Divide-and-conquer algorithm) algorithm and recursive
+     * Time Complexity (Log n)
+     *
+     * @param array
+     * @param key
+     * @param low
+     * @param high
+     * @return
+     */
+    public static int binarySearchByRecursive(int array[], int key, int low, int high) {
+        if (low > high)
+            return 0;
+        else {
+            int mid = (low + high) / 2;
+            if (key == array[mid])
+                return mid;
+            else if (key < array[mid])
+                // Tail recursive call
+                return binarySearchByRecursive(array, key, low, mid - 1);
+            else
+                // // Tail recursive call
+                return binarySearchByRecursive(array, key, mid + 1, high);
+        }
+    }
 }
