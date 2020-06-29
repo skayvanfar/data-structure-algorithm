ca7229538ee1de0d0b61e01b7158fb27a6787921
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-06-29 20:20:47 +0430

    decreaseAndConquerByRecursive is added.

--- data-structure/src/main/java/ir/sk/algorithm/FindPeakElement.java
+++ data-structure/src/main/java/ir/sk/algorithm/FindPeakElement.java
@@ -1,5 +1,9 @@
 package ir.sk.algorithm;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by sad.keyvanfar on 6/27/2020.
  */
@@ -57,6 +61,7 @@ public class FindPeakElement {
 
     /**
      * Time Complexity: O(mn)
+     *
      * @param array
      * @param low
      * @param high
@@ -74,7 +79,7 @@ public class FindPeakElement {
         for (int i = 0; i < array.length; i++) {
             for (int j = 1; j < array[i].length; j++) {
                 if (array[i][0] >= array[i][1] && array[i][0] >= array[i - 1][0] && array[i][0] >= array[i + 1][0]
-                            && array[i][array.length-1] >= array[i][array.length-2] && array[i][array.length-1] >= array[i - 1][array.length-1] && array[i][array.length-1] >= array[i + 1][array.length-1])
+                        && array[i][array.length - 1] >= array[i][array.length - 2] && array[i][array.length - 1] >= array[i - 1][array.length - 1] && array[i][array.length - 1] >= array[i + 1][array.length - 1])
                     return true;
             }
         }
@@ -95,7 +100,7 @@ public class FindPeakElement {
      * Greedy Ascent Algorithm works on the principle, that it selects a particular element to start with.
      * Then it begins traversing across the array, by selecting the neighbour with higher value.
      * If there is no neighbour with a higher value than the current element, it just returns the current element.
-     *
+     * <p>
      * Time Complexity: O(mn)
      *
      * @param array
@@ -103,11 +108,40 @@ public class FindPeakElement {
      * @param j
      * @return
      */
-    public static int greedyAscentAlgorithmByRecursive(int [][] array, int i, int j) {
+    public static int greedyAscentAlgorithmByRecursive(int[][] array, int i, int j) {
         if (i > 0 && array[i - 1][j] > array[i][j]) return greedyAscentAlgorithmByRecursive(array, i - 1, j);
-        else if (i < array.length - 1 && array[i + 1][j] > array[i][j]) return greedyAscentAlgorithmByRecursive(array, i + 1, j);
+        else if (i < array.length - 1 && array[i + 1][j] > array[i][j])
+            return greedyAscentAlgorithmByRecursive(array, i + 1, j);
         else if (j > 0 && array[i][j - 1] > array[i][j]) return greedyAscentAlgorithmByRecursive(array, i, j - 1);
-        else if (j < array.length - 1 && array[i][j + 1] > array[i][j]) return greedyAscentAlgorithmByRecursive(array, i, j + 1);
+        else if (j < array.length - 1 && array[i][j + 1] > array[i][j])
+            return greedyAscentAlgorithmByRecursive(array, i, j + 1);
         else return array[i][j];
     }
+
+    /**
+     * Time Complexity : O(rows * log(columns))
+     * Auxiliary Space: O(columns/2) for Recursion Call Stack
+     * @param array
+     * @param col1
+     * @param col2
+     * @return
+     */
+    public static int decreaseAndConquerByRecursive(int[][] array, int col1, int col2) {
+        int mid = (col1 + col2) / 2;
+        int max = findMaxIndex(array, mid);
+        if (col1 == col2)
+            return array[max][mid];
+        if (array[max][mid] < array[max][mid+1])
+            return decreaseAndConquerByRecursive(array, mid + 1, col2);
+        else if (array[max][mid] > array[max][mid-1])
+            return decreaseAndConquerByRecursive(array, col1, mid + 1);
+        else
+            return array[max][mid];
+    }
+
+    private static int findMaxIndex(int[][] array, int col) {
+        List<Integer> integers = Arrays.stream(array).map(ints -> ints[col]).collect(Collectors.toList());
+        return integers.indexOf(integers.stream().max(Integer::compareTo).get());
+    }
+
 }
--- data-structure/src/test/java/ir/sk/algorithm/FindPeakElementTest.java
+++ data-structure/src/test/java/ir/sk/algorithm/FindPeakElementTest.java
@@ -10,8 +10,15 @@ import org.junit.Test;
  */
 public class FindPeakElementTest {
 
+    private int[] array;
+    private int[][] array2D;
+
     @Before
     public void setUp() throws Exception {
+        array = new int[]{1, 5, 2, 4, 9, 7};
+        array2D = new int[][]{{1, 5, 6},
+                {2, 4, 1},
+                {6, 4, 6}};
     }
 
     @After
@@ -21,48 +28,52 @@ public class FindPeakElementTest {
 
     @Test
     public void findPeakByLoop() {
-        int[] array = new int[] {1,5,2,4,9,7};
         boolean expectedValue = true;
         long startTime = System.nanoTime();
         boolean result = FindPeakElement.findPeakByLoop(array, 0, array.length);
         long endTime = System.nanoTime();
-        System.out.println("time duration for findPeakByLoop by array size: "+array.length + " = "+(endTime-startTime)+ " nano");
+        System.out.println("time duration for findPeakByLoop by array size: " + array.length + " = " + (endTime - startTime) + " nano");
         Assert.assertEquals(expectedValue, result);
     }
 
     @Test
     public void findPeakByRecursive() {
-        int[] array = new int[] {1,5,2,4,9,7};
         boolean expectedValue = true;
         long startTime = System.nanoTime();
         boolean result = FindPeakElement.findPeakByRecursive(array, 0, array.length);
         long endTime = System.nanoTime();
-        System.out.println("time duration for fibonacciByArray by array size: "+array.length + " = "+(endTime-startTime)+ " nano");
+        System.out.println("time duration for fibonacciByArray by array size: " + array.length + " = " + (endTime - startTime) + " nano");
         Assert.assertEquals(expectedValue, result);
     }
 
     @Test
     public void findPeakIn2DArrayByLoop() {
-        int[][] array = new int[][] {{1,5,6},{2,4,1},{9,7,9}};
         boolean expectedValue = true;
         long startTime = System.nanoTime();
-        boolean result = FindPeakElement.findPeakIn2DArrayByLoop(array, 0, array.length);
+        boolean result = FindPeakElement.findPeakIn2DArrayByLoop(array2D, 0, array.length);
         long endTime = System.nanoTime();
-        System.out.println("time duration for findPeakIn2DArrayByLoop by array size: "+array.length + " = "+(endTime-startTime)+ " nano");
+        System.out.println("time duration for findPeakIn2DArrayByLoop by array size: " + array2D.length + " = " + (endTime - startTime) + " nano");
         Assert.assertEquals(expectedValue, result);
     }
 
     @Test
     public void greedyAscentAlgorithmByRecursive() {
-        int[][] array = new int[][] {{1,5,6},
-                                     {2,4,1},
-                                     {6,4,6}};
         int expectedValue = 6;
         long startTime = System.nanoTime();
-        int result = FindPeakElement.greedyAscentAlgorithmByRecursive(array, 0, 0);
+        int result = FindPeakElement.greedyAscentAlgorithmByRecursive(array2D, 0, 0);
         long endTime = System.nanoTime();
-        System.out.println("time duration for greedyAscentAlgorithmByRecursive by array size: "+array.length + " = "+(endTime-startTime)+ " nano");
+        System.out.println("time duration for greedyAscentAlgorithmByRecursive by array size: " + array2D.length + " = " + (endTime - startTime) + " nano");
         Assert.assertEquals(expectedValue, result);
 
+    }
+
+    @Test
+    public void decreaseAndConquerByRecursive() {
+        int expectedValue = 6;
+        long startTime = System.nanoTime();
+        int result = FindPeakElement.decreaseAndConquerByRecursive(array2D, 0, array2D[0].length - 1);
+        long endTime = System.nanoTime();
+        System.out.println("time duration for decreaseAndConquerByRecursive by array size: " + array2D.length + " = " + (endTime - startTime) + " nano");
+        Assert.assertEquals(expectedValue, result);
     }
 }
