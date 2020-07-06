f3ab268fc93c7860270cc104b69f3970b36ecf4a
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-07-06 20:11:59 +0430

    fixes

--- data-structure/src/test/java/ir/sk/datastructure/fundamental/hashing/DictionaryTest.java
+++ data-structure/src/test/java/ir/sk/datastructure/fundamental/hashing/DictionaryTest.java
@@ -1,13 +1,9 @@
 package ir.sk.datastructure.fundamental.hashing;
 
-import ir.sk.algorithm.Search;
-import ir.sk.algorithm.Sort;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * Created by sad.keyvanfar on 7/6/2020.
@@ -18,11 +14,18 @@ public class DictionaryTest {
 
     @Before
     public void setUp() throws Exception {
-        dictionary = new Dictionary<>();
-        dictionary.add("this",1 );
-        dictionary.add("coder",2 );
-        dictionary.add("this",4 );
-        dictionary.add("hi",5 );
+        dictionary = new Dictionary<>(10, 0.5f);
+        dictionary.add("a",1 );
+        dictionary.add("b",2 );
+        dictionary.add("c",3 );
+        dictionary.add("d",4 );
+        dictionary.add("e",5 );
+        dictionary.add("f",6 );
+        dictionary.add("g",7 );
+        dictionary.add("h",8 );
+        dictionary.add("l",9 );
+        dictionary.add("m",10 );
+        dictionary.add("c",30 );
     }
 
     @After
@@ -31,9 +34,9 @@ public class DictionaryTest {
 
     @Test
     public void get() {
-        int expectedValue = 2;
+        int expectedValue = 7;
         long startTime = System.nanoTime();
-        int resultIndex = dictionary.get("coder");
+        int resultIndex = dictionary.get("g");
         long endTime = System.nanoTime();
         System.out.println("time duration for DictionaryTest.get() with n: " + dictionary.size() + " length = " + (endTime - startTime) + " nano");
         Assert.assertEquals(expectedValue, resultIndex);
@@ -46,7 +49,7 @@ public class DictionaryTest {
 
     @Test
     public void remove() {
-        dictionary.remove("coder");
+        dictionary.remove("g");
     }
 
 }
