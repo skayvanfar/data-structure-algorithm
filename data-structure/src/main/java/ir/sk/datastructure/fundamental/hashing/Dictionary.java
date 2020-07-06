198b017f2dfd240b6ce71feeebfe7d466eac9709
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-07-06 16:57:38 +0430

    Dictionary is added.

---
+++ data-structure/src/main/java/ir/sk/datastructure/fundamental/hashing/Dictionary.java
@@ -0,0 +1,202 @@
+package ir.sk.datastructure.fundamental.hashing;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
+/**
+ * Dictionary ADT (HashTable ADT) (HashMap ADT)
+ * Hash table is a generalization of array. With an array, we store the element whose key is k at a
+ * position k of the array.That means, given a key k, we find the element whose key is k by just
+ * looking in the kth position of the array. This is called direct addressing.
+ * Direct addressing is applicable when we can afford to allocate an array with one position for
+ * every possible key.
+ *
+ * Hash table or hash map is a data structure that
+ * stores the keys and their associated values, and hash table uses a hash function to map keys to
+ * their associated values.
+ *
+ * Created by sad.keyvanfar on 7/6/2020.
+ */
+public class Dictionary<K, V> {
+
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    // bucketArray is used to store array of chains
+    private HashNode<K, V>[] bucketArray;
+
+    // Current capacity of array list
+    private int numBuckets;
+
+    // Current size of array list
+    private int size;
+
+    private final float loadFactor;
+
+    public Dictionary() {
+        this.loadFactor = DEFAULT_LOAD_FACTOR;
+        this.numBuckets = DEFAULT_CAPACITY;
+        this.size = 0;
+        bucketArray = new HashNode[numBuckets];
+    }
+
+    public Dictionary(int initialCapacity, float loadFactor) {
+        if (initialCapacity < 0)
+            throw new IllegalArgumentException("Illegal initial capacity: " + initialCapacity);
+        if (loadFactor <= 0 || Float.isNaN(loadFactor))
+            throw new IllegalArgumentException("Illegal load factor: " + loadFactor);
+        this.loadFactor = loadFactor;
+        this.numBuckets = initialCapacity;
+        this.size = 0;
+        bucketArray = new HashNode[numBuckets];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * This implements hash function to find index for a key
+     *
+     * @param key
+     * @return
+     */
+    private int hashFuntion(K key) {
+        // prehashing
+        int hashCode = key.hashCode();
+        // hashing
+        int index = hashCode % numBuckets;
+        return index;
+    }
+
+    /**
+     * Returns value for a key
+     *
+     * @param key
+     * @return
+     */
+    public V get(K key) {
+
+        // Find head of chain for given key
+        int bucketIndex = hashFuntion(key);
+        HashNode<K, V> head = bucketArray[bucketIndex];
+
+        // Search key in chain
+        while (head != null) {
+            if (head.key.equals(key))
+                return head.value;
+            head = head.next;
+        }
+
+        return null;
+    }
+
+    /**
+     * Adds a key value pair to hash
+     *
+     * @param key
+     * @param value
+     */
+    public void add(K key, V value) {
+        // Find head of chain for given key
+        int bucketIndex = hashFuntion(key);
+        HashNode<K, V> head = bucketArray[bucketIndex];
+
+        // Check if key is already present
+        while (head != null) {
+            if (head.key.equals(key)) {
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+
+        // Insert key in chain
+        size++;
+        head = bucketArray[bucketIndex];
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        newNode.next = head;
+        bucketArray[bucketIndex] = newNode;
+
+        reHash();
+    }
+
+    /**
+     * If load factor goes beyond threshold, then
+     * double hash table size
+     */
+    private void reHash() {
+        if ((1.0 * size) / numBuckets >= this.loadFactor) {
+            numBuckets = 2 * numBuckets;
+            size = 0;
+            HashNode<K, V>[] temp = bucketArray;
+            bucketArray = new HashNode[numBuckets];
+
+            for (HashNode<K, V> headNode : temp) {
+                while (headNode != null) {
+                    add(headNode.key, headNode.value);
+                    headNode = headNode.next;
+                }
+            }
+        }
+    }
+
+    /**
+     * Method to remove a given key
+     *
+     * @param key
+     * @return
+     */
+    public V remove(K key) {
+        // Apply hash function to find index for given key
+        int bucketIndex = hashFuntion(key);
+
+        // Get head of chain
+        HashNode<K, V> head = bucketArray[bucketIndex];
+
+        // Search for key in its chain
+        HashNode<K, V> prev = null;
+        while (head != null) {
+            // If Key found
+            if (head.key.equals(key))
+                break;
+
+            // Else keep moving in chain
+            prev = head;
+            head = head.next;
+        }
+
+        // If key was not there
+        if (head == null)
+            return null;
+
+        // Reduce size
+        size--;
+
+        // Remove key
+        if (prev != null)
+            prev.next = head.next;
+        else
+            bucketArray[bucketIndex] = head.next;
+
+        return head.value;
+    }
+}
+
+class HashNode<K, V> {
+
+    K key;
+    V value;
+
+    // Reference to next node
+    HashNode<K, V> next;
+
+    public HashNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
---
+++ data-structure/src/test/java/ir/sk/datastructure/fundamental/hashing/DictionaryTest.java
@@ -0,0 +1,46 @@
+package ir.sk.datastructure.fundamental.hashing;
+
+import ir.sk.algorithm.Search;
+import ir.sk.algorithm.Sort;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sad.keyvanfar on 7/6/2020.
+ */
+public class DictionaryTest {
+
+    private Dictionary<String, Integer> dictionary;
+
+    @Before
+    public void setUp() throws Exception {
+        dictionary = new Dictionary<>();
+        dictionary.add("this",1 );
+        dictionary.add("coder",2 );
+        dictionary.add("this",4 );
+        dictionary.add("hi",5 );
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void get() {
+        int expectedValue = 2;
+        long startTime = System.nanoTime();
+        int resultIndex = dictionary.get("coder");
+        long endTime = System.nanoTime();
+        System.out.println("time duration for DictionaryTest.get() with n: " + dictionary.size() + " length = " + (endTime - startTime) + " nano");
+        Assert.assertEquals(expectedValue, resultIndex);
+    }
+
+    @Test
+    public void add() {
+        dictionary.get("coder");
+    }
+}
