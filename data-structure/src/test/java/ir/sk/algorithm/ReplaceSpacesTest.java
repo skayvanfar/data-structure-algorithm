1c7a20da813cd5a9d194981ff62056d2a2d21c7e
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-08-25 15:54:36 +0430

    ReplaceSpaces is added.

---
+++ data-structure/src/test/java/ir/sk/algorithm/ReplaceSpacesTest.java
@@ -0,0 +1,31 @@
+package ir.sk.algorithm;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sad.kayvanfar on 8/25/2020.
+ */
+public class ReplaceSpacesTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void replaceSpaces() {
+        char[] str = "Saeed Kayvanfar    ".toCharArray();
+
+        str = ReplaceSpaces.replaceSpaces(str);
+
+        for (int i = 0; i < str.length; i++)
+            System.out.print(str[i]);
+    }
+}
