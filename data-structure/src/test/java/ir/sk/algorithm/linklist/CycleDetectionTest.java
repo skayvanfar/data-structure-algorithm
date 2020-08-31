31038b0e7fa1676420cd717987e6258ecf60d4b7
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-08-31 21:54:20 +0430

    CycleDetection is added.

---
+++ data-structure/src/test/java/ir/sk/algorithm/linklist/CycleDetectionTest.java
@@ -0,0 +1,29 @@
+package ir.sk.algorithm.linklist;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author <a href="kayvanfar.sj@gmail.com">Saeed Kayvanfar</a> on 8/31/2020.
+ */
+public class CycleDetectionTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void detectCycleByHashing() {
+    }
+
+    @Test
+    public void floydsCycleFinding() {
+    }
+}
