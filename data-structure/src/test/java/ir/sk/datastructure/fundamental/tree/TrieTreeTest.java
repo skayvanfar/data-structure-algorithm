a06d290383429f8427c0685ec80bcef98d2426f9
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-09-03 17:38:30 +0430

    TrieTree is added.

---
+++ data-structure/src/test/java/ir/sk/datastructure/fundamental/tree/TrieTreeTest.java
@@ -0,0 +1,43 @@
+package ir.sk.datastructure.fundamental.tree;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * A trie (also known as a digital tree) and sometimes even radix tree or prefix tree (as they can be searched by prefixes), is an ordered tree structure, which takes advantage of the keys that it stores – usually strings.
+ *
+ * A node's position in the tree defines the key with which that node is associated, which makes tries different in comparison to binary search trees, in which a node stores a key that corresponds only to that node.
+ *
+ * All descendants of a node have a common prefix of a String associated with that node, whereas the root is associated with an empty String.
+ *
+ * @author <a href="kayvanfar.sj@gmail.com">Saeed Kayvanfar</a> on 9/3/2020.
+ */
+public class TrieTreeTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void containsNode() {
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+}
