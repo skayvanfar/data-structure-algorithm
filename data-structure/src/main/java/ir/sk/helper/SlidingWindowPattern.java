01b6bf28c9c6b4ea0653117896027c17ce4fc7eb
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-09-06 13:48:40 +0430

    three patterns are added.

---
+++ data-structure/src/main/java/ir/sk/helper/SlidingWindowPattern.java
@@ -0,0 +1,11 @@
+package ir.sk.helper;
+
+/**
+ * Involves creating a window from an array or number from one position to another.
+ * Depending on certain conditions the window will either increase or decrease the window.
+ * It is useful for keeping track of a subset of data.
+ *
+ * Created by sad.kayvanfar on 9/6/2020.
+ */
+public @interface SlidingWindowPattern {
+}
