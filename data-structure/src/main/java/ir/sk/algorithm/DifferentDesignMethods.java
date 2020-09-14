2131c7a2351772c0deeeb37aaf7f161383ff7f08
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-09-14 11:13:24 +0430

    fixes

--- data-structure/src/main/java/ir/sk/algorithm/DifferentDesignMethods.java
+++ data-structure/src/main/java/ir/sk/algorithm/DifferentDesignMethods.java
@@ -136,41 +136,25 @@ public class DifferentDesignMethods {
     }
 
     /**
-     * @param str
+     * @param input
      * @return
      */
     @SlidingWindowPattern
     @TimeComplexity("O(n)")
-    public static int longestUniqueSubsttr(String str)
-    {
-        int n = str.length();
-
-        int res = 0; // result
-
-        // last index of all characters is initialized
-        // as -1
-        int [] lastIndex = new int[256];
-        Arrays.fill(lastIndex, -1);
-
-        // Initialize start of current window
-        int i = 0;
-
-        // Move end of current window
-        for (int j = 0; j < n; j++) {
-
-            // Find the last index of str[j]
-            // Update i (starting index of current window)
-            // as maximum of current value of i and last
-            // index plus 1
-            i = Math.max(i, lastIndex[str.charAt(j)] + 1);
-
-            // Update result if we get a larger window
-            res = Math.max(res, j - i + 1);
-
-            // Update last index of j.
-            lastIndex[str.charAt(j)] = j;
+    String getUniqueCharacterSubstring(String input) {
+        Map<Character, Integer> visited = new HashMap<>();
+        String output = "";
+        for (int start = 0, end = 0; end < input.length(); end++) {
+            char currChar = input.charAt(end);
+            if (visited.containsKey(currChar)) {
+                start = Math.max(visited.get(currChar)+1, start);
+            }
+            if (output.length() < end - start + 1) {
+                output = input.substring(start, end + 1);
+            }
+            visited.put(currChar, end);
         }
-        return res;
+        return output;
     }
 
 }
