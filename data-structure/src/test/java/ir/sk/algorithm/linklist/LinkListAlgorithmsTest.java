7ca6367c11b2bc04fdc1459d8d227736712de5ac
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-09-16 15:46:14 +0430

    LinkListAlgorithmsTest is added.

---
+++ data-structure/src/test/java/ir/sk/algorithm/linklist/LinkListAlgorithmsTest.java
@@ -0,0 +1,37 @@
+package ir.sk.algorithm.linklist;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sad.kayvanfar on 9/16/2020.
+ */
+public class LinkListAlgorithmsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void deleteDuplicatesByRunner() {
+    }
+
+    @Test
+    public void deleteDuplicatesByHashing() {
+    }
+
+    @Test
+    public void kthToLastRecursive() {
+    }
+
+    @Test
+    public void sumRevertedLists() {
+    }
+}
