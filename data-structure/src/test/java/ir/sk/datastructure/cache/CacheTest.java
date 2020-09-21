bd725920877f35d9d5922a6b1c051426468ce5b7
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-09-21 13:19:00 +0330

    Cache is added.

---
+++ data-structure/src/test/java/ir/sk/datastructure/cache/CacheTest.java
@@ -0,0 +1,54 @@
+package ir.sk.datastructure.cache;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sad.kayvanfar on 9/21/2020.
+ */
+public class CacheTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void cache() {
+        Cache cache = new Cache();
+        for (int i = 0; i < 20; i++) {
+            String query = "query" + i;
+            cache.insertResults(query, generateResults(i));
+            if (i == 9 || i == 16 || i == 19) {
+                cache.getResults("query" + 2);
+                cache.getResults("query" + 6);
+                cache.getResults("query" + 9);
+            }
+        }
+
+        for (int i = 0; i < 30; i++) {
+            String query = "query" + i;
+            String[] results = cache.getResults(query);
+            System.out.print(query + ": ");
+            if (results == null) {
+                System.out.print("null");
+            } else {
+                for (String s : results) {
+                    System.out.print(s + ", ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public static String[] generateResults(int i) {
+        String[] results =  {"resultA" + i, "resultB" + i, "resultC" + i};
+        return results;
+    }
+}
