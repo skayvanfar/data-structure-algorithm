0909e5911f1d51b9a58a27f403dd58747c832869
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-09-22 15:11:20 +0330

    TripleStep is added.

---
+++ data-structure/src/main/java/ir/sk/algorithm/TripleStep.java
@@ -0,0 +1,72 @@
+package ir.sk.algorithm;
+
+import ir.sk.helper.BruteForce;
+import ir.sk.helper.Memoization;
+import ir.sk.helper.SpaceComplexity;
+import ir.sk.helper.TimeComplexity;
+
+/**
+ * A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3
+ * steps at a time. Implement a method to count how many possible ways the child can run up the
+ * stairs.
+ * <p>
+ * Created by sad.kayvanfar on 9/22/2020.
+ */
+public class TripleStep {
+
+    /**
+     * @param n
+     * @return
+     */
+    @TimeComplexity("O(n^3) O(branches ^ depth)")
+    @SpaceComplexity("O(1)")
+    @BruteForce
+    public static int naiveCountWays(int n) {
+        if (n < 0)
+            return 0;
+        else if (n == 0)
+            return 1;
+        else
+            return naiveCountWays(n - 1) + naiveCountWays(n - 2) + naiveCountWays(n - 3);
+    }
+
+    public static int memoizedDPCountWaysByRecursive(int n) {
+        int[] memo = new int[n];
+        return memoizedDPCountWaysByRecursive(n, memo);
+    }
+
+    @TimeComplexity("O(n)")
+    @SpaceComplexity("O(n)")
+    @Memoization
+    private static int memoizedDPCountWaysByRecursive(int n, int[] memo) {
+        if (n < 0)
+            return 0;
+        else if (n == 0)
+            return 1;
+        else if (memo[n] > -1)
+            return memo[n];
+        else {
+            memo[n] = memoizedDPCountWaysByRecursive(n - 1, memo) + memoizedDPCountWaysByRecursive(n - 2, memo) +
+                    memoizedDPCountWaysByRecursive(n - 3, memo);
+            return memo[n];
+        }
+    }
+
+    // A recursive function used by countWays
+    @TimeComplexity("O(n)")
+    @SpaceComplexity("O(1)")
+    public static int bottomUpCountWays(int n) {
+        int[] res = new int[n + 1];
+        res[0] = 1;
+        res[1] = 1;
+        res[2] = 2;
+
+        for (int i = 3; i <= n; i++)
+            res[i] = res[i - 1] + res[i - 2]
+                    + res[i - 3];
+
+        return res[n];
+    }
+
+
+}
---
+++ data-structure/src/test/java/ir/sk/algorithm/TripleStepTest.java
@@ -0,0 +1,33 @@
+package ir.sk.algorithm;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sad.kayvanfar on 9/22/2020.
+ */
+public class TripleStepTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void naiveCountWays() {
+    }
+
+    @Test
+    public void memoizedDPCountWaysByRecursive() {
+    }
+
+    @Test
+    public void bottomUpCountWays() {
+    }
+}
