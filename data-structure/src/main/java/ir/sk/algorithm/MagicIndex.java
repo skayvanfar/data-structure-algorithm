6e6fcac6ecee045d3abb53bec60509b89c8ac724
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-09-22 15:27:26 +0330

    MagicIndex is added.

---
+++ data-structure/src/main/java/ir/sk/algorithm/MagicIndex.java
@@ -0,0 +1,50 @@
+package ir.sk.algorithm;
+
+import ir.sk.helper.BruteForce;
+import ir.sk.helper.TimeComplexity;
+
+/**
+ * A magic index in an array A[ 1 .•. n-1] is defined to be an index such that A[ i]
+ * i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in
+ * array A.
+ * <p>
+ * Created by sad.kayvanfar on 9/22/2020.
+ */
+public class MagicIndex {
+
+    /**
+     * @param array
+     * @return
+     */
+    @TimeComplexity("O(n)")
+    @BruteForce
+    public static int magicSlow(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == i)
+                return i;
+        }
+        return -1;
+    }
+
+    /**
+     * @param array
+     * @return
+     */
+    @TimeComplexity("O(Log n)")
+    public static int magicFast(int[] array) {
+        return magicFast(array, 0, array.length - 1);
+    }
+
+    private static int magicFast(int[] array, int start, int end) {
+        if (end < start)
+            return -1;
+
+        int mid = (start + end) / 2;
+        if (array[mid] == mid)
+            return mid;
+        else if (array[mid] > mid)
+            return magicFast(array, start, mid - 1);
+        else
+            return magicFast(array, mid + 1, end);
+    }
+}
---
+++ data-structure/src/test/java/ir/sk/algorithm/MagicIndexTest.java
@@ -0,0 +1,29 @@
+package ir.sk.algorithm;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sad.kayvanfar on 9/22/2020.
+ */
+public class MagicIndexTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void magicSlow() {
+    }
+
+    @Test
+    public void magicFast() {
+    }
+}
