9ecae6902c9dde5fa912d6ab0daa6dbeced94f20
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-09-22 15:49:56 +0330

    magicFast is added.

--- data-structure/src/main/java/ir/sk/algorithm/TripleStep.java
+++ data-structure/src/main/java/ir/sk/algorithm/TripleStep.java
@@ -68,5 +68,44 @@ public class TripleStep {
         return res[n];
     }
 
+    /**
+     * elements are not distinct
+     *
+     * @param array
+     * @return
+     */
+    public static int magicFast(int[] array) {
+        return magicFast(array, 0, array.length - 1);
+    }
+
+    /**
+     * elements are not distinct
+     * @param array
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int magicFast(int[] array, int start, int end) {
+        if (end < start) return -1;
+
+        int midindex = (start + end) / 2;
+        int midValue = array[midindex];
+        if (midValue == midindex)
+            return midindex;
+
+        /* Search left */
+        int leftindex = Math.min(midindex - 1, midValue);
+        int left = magicFast(array, start, leftindex);
+        if (left >= 0)
+            return left;
+
+
+        /* Search right */
+        int rightIndex = Math.max(midindex + 1, midValue);
+        int right = magicFast(array, rightIndex, end);
+
+        return right;
+    }
+
 
 }
