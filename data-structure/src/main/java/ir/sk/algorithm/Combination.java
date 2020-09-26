73c1b74987fc581abf663c1b290de2e84b62a3b4
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-09-26 12:03:35 +0330

    fixes

--- data-structure/src/main/java/ir/sk/algorithm/Combination.java
+++
@@ -1,9 +0,0 @@
-package ir.sk.algorithm;
-
-/**
- * @author <a href="kayvanfar.sj@gmail.com">Saeed Kayvanfar</a> on 2/6/2020.
- */
-public class Combination {
-
-
-}
--- data-structure/src/main/java/ir/sk/datastructure/fundamental/array/Array.java
+++ data-structure/src/main/java/ir/sk/datastructure/fundamental/array/Array.java
@@ -1,5 +1,7 @@
 package ir.sk.datastructure.fundamental.array;
 
+import ir.sk.helper.TimeComplexity;
+
 /**
  * Unordered array data structure like ArrayList in java without using index. Duplicate is allowed.
  * When duplicate is not allowed, It's like Set in java
@@ -10,23 +12,24 @@ package ir.sk.datastructure.fundamental.array;
  */
 public class Array {
 
-    private int[] a;                     // ref to array a
+    private int[] array;                     // ref to array a
     private int num;                        // number of data items
 
     public Array(int max) {
-        a = new int[max];                // create the array
+        array = new int[max];                // create the array
         num = 0;                            // no items yet
     }
 
     /**
-     * Time Complexity: O(n)
+     *
      * @param searchKey
      * @return
      */
+    @TimeComplexity("O(n)")
     public boolean find(int searchKey) {
         int j;
         for(j = 0; j < num; j++)            // for each element, Linear Search
-            if(a[j] == searchKey)           // found item?
+            if(array[j] == searchKey)           // found item?
                 break;                      // exit loop before end
         if(j == num)                        // gone to end?
             return false;                   // yes, can't find it
@@ -37,31 +40,31 @@ public class Array {
     /**
      * put element into array
      *
-     * Time Complexity: O(1)
-     *
      * @param value
      */
+    @TimeComplexity("O(1)")
     public void insert(int value) {
-        a[num] = value;                     // insert it
+        array[num] = value;                     // insert it
         num++;                              // increment size
     }
 
     /**
-     * Time Complexity: O(n)
+     * after delete element, move all elements after index location (shift)
      * @param value
      * @return
      */
+    @TimeComplexity("O(n)")
     public boolean delete(int value) {
         int j;
         for(j = 0; j < num; j++)            // look for it
-            if(value == a[j])
+            if(value == array[j])
                 break;
         if(j == num)                        // can't find it
             return false;
         else {                              // found it
             //shifting
             for(int k = j; k < num; k++)    // move higher ones down
-                a[k] = a[k+1];
+                array[k] = array[k+1];
             num--;                          // decrement size
             return true;
         }
@@ -72,7 +75,7 @@ public class Array {
      */
     public void display() {
         for(int j=0; j<num; j++)            // for each element,
-            System.out.print(a[j] + " ");   // display it
+            System.out.print(array[j] + " ");   // display it
         System.out.println("");
     }
 }
