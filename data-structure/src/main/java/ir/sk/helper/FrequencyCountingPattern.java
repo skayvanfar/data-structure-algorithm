d80b45b6b19f285157bdf6305deeea9dc88f5dae
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-10-26 12:09:49 +0330

    fixes

--- data-structure/src/main/java/ir/sk/helper/FrequencyCountingPattern.java
+++ data-structure/src/main/java/ir/sk/helper/FrequencyCountingPattern.java
@@ -7,6 +7,8 @@ package ir.sk.helper;
  *
  * if hashtable use, Using hashtable is trade off between time and space change Multiply O(n)*O(n) into O(n)+O(n) by using hashtable
  *
+ * This can often avoid the need for nested loops or O(n^2) operations with arrays/strings.
+ *
  * there are different ADT you can use, like int array, boolean array(in case most frequency is 1), Map(saving other frequency), BitSet, int,
  * Of Course you can use other data structures like TreeSet instead of hashtable, but Time complexity decrease into O(Log n).
  * <p>
