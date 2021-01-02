21a1a3d300cdff7720c6ac67840f1f7fc3907744
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-01-02 19:01:40 +0330

    ExpressionTree is added.

---
+++ data-structure/src/test/java/ir/sk/algorithm/treeandgraph/ExpressionTreeTest.java
@@ -0,0 +1,34 @@
+package ir.sk.algorithm.treeandgraph;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author <a href="kayvanfar.sj@gmail.com">Saeed Kayvanfar</a> on 1/2/2021.
+ */
+public class ExpressionTreeTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void calculateExpression() {
+        Node<Character> root =
+                new Node<>('*',
+                        new Node<>('+',
+                                new Node<>('3'),
+                                new Node<>('2')),
+                        new Node<>('+',
+                                new Node<>('4'),
+                                new Node<>('5')));
+        System.out.println(ExpressionTree.calculateExpression(root));
+    }
+}
