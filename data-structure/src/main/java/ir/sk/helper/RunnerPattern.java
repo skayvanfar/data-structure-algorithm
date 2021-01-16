248c858f7ff67994f592bbb22484117f78341e07
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-01-16 13:35:24 +0330

    fixes

--- data-structure/src/main/java/ir/sk/helper/RunnerPattern.java
+++ data-structure/src/main/java/ir/sk/helper/RunnerPattern.java
@@ -1,7 +1,7 @@
 package ir.sk.helper;
 
 /**
- * (Runner Pattern) The Fast & Slow pointer approach, also known as the Hare & Tortoise algorithm, is a pointer algorithm that uses two pointers which move through the array (or sequence/LinkedList) at different speeds.
+ * (Runner Pattern) The Fast & Slow pointer approach, also known as the Hare & Tortoise algorithm, is a pointer algorithm (MultiplePointerPattern) that uses two pointers which move through the array (or sequence/LinkedList) at different speeds.
  * This approach is quite useful when dealing with cyclic LinkedLists or arrays.
  *
  * By moving at different speeds (say, in a cyclic LinkedList), the algorithm proves that the two pointers are bound to meet. The fast pointer should catch the slow pointer once both the pointers are in a cyclic loop.
