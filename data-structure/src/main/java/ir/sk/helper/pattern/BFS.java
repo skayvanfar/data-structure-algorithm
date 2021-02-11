3f8a8a976c4afa259dba874a5336bfffc0af14e7
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-02-11 06:33:00 +0330

    fixes

--- data-structure/src/main/java/ir/sk/helper/pattern/BFS.java
+++ data-structure/src/main/java/ir/sk/helper/pattern/BFS.java
@@ -1,7 +1,9 @@
 package ir.sk.helper.pattern;
 
 /**
- * Breadth First Search (BFS) approach
+ * Breadth First Search (BFS) approach (Subsets)
+ * Starting with an empty collection(Set, Queue, ...),
+ * iterate through all numbers one-by-one, and do something on the collection.
  *
  * @author <a href="kayvanfar.sj@gmail.com">Saeed Kayvanfar</a> on 2/10/2021.
  */
