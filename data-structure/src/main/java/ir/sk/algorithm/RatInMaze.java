cea141602a026cb012e065348db76b23d862fc8e
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-02-15 13:22:00 +0330

    fixes

--- data-structure/src/main/java/ir/sk/algorithm/RatInMaze.java
+++ data-structure/src/main/java/ir/sk/algorithm/RatInMaze.java
@@ -1,5 +1,7 @@
 package ir.sk.algorithm;
 
+import ir.sk.helper.complexity.SpaceComplexity;
+import ir.sk.helper.complexity.TimeComplexity;
 import ir.sk.helper.technique.Backtracking;
 
 /**
@@ -24,6 +26,8 @@ import ir.sk.helper.technique.Backtracking;
  * Created by sad.kayvanfar on 2/15/2021.
  */
 @Backtracking
+@TimeComplexity("O(2^(n^2))")
+@SpaceComplexity("O(n^2)")
 public class RatInMaze {
 
     // size of the maze
