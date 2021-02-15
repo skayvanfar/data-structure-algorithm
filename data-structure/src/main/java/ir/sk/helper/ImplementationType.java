72b25ffc3fb3f7fa39308315c7cdebdc72a12dbf
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-02-15 21:01:13 +0330

    fixes

--- data-structure/src/main/java/ir/sk/helper/ImplementationType.java
+++ data-structure/src/main/java/ir/sk/helper/ImplementationType.java
@@ -4,6 +4,6 @@ package ir.sk.helper;
  * Created by sad.kayvanfar on 2/6/2021.
  */
 public enum ImplementationType {
-    Iterative,
-    Recursive
+    Iterative, // (Down-Top)
+    Recursive // (Top-Down)
 }
