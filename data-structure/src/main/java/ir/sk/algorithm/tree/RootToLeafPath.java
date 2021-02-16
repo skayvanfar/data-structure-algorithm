68ad0be047280b50b2866c8db5a15a1481924d2b
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-02-16 14:26:48 +0330

    findDiameter is added.

--- data-structure/src/main/java/ir/sk/algorithm/tree/RootToLeafPath.java
+++ data-structure/src/main/java/ir/sk/algorithm/tree/RootToLeafPath.java
@@ -277,4 +277,49 @@ public class RootToLeafPath {
         return count;
     }
 
+    private static int treeDiameter = 0;
+
+    public static int findDiameter(TreeNode root) {
+        calculateHeight(root);
+        return treeDiameter;
+    }
+
+    /**
+     * Given a binary tree, find the length of its diameter.
+     * The diameter of a tree is the number of nodes on the longest path between any two leaf nodes.
+     * The diameter of a tree may or may not pass through the root.
+     *
+     * At every step, we need to find the height of both children of the current node. For this, we will make two recursive calls similar to DFS.
+     * The height of the current node will be equal to the maximum of the heights of its left or right children, plus ‘1’ for the current node.
+     * The tree diameter at the current node will be equal to the height of the left child plus the height of the right child plus ‘1’ for the current node: diameter = leftTreeHeight + rightTreeHeight + 1.
+     * To find the overall tree diameter, we will use a class level variable.
+     * This variable will store the maximum diameter of all the nodes visited so far, hence, eventually,
+     * it will have the final tree diameter.
+     *
+     * Note: You can always assume that there are at least two leaf nodes in the given tree.
+     *
+     * @param currentNode
+     * @return
+     */
+    @TimeComplexity("O(n)")
+    @SpaceComplexity("O(n)")
+    @BacktrackingDFS
+    private static int calculateHeight(TreeNode currentNode) {
+        if (currentNode == null)
+            return 0;
+
+        int leftTreeHeight = calculateHeight(currentNode.left);
+        int rightTreeHeight = calculateHeight(currentNode.right);
+
+        // diameter at the current node will be equal to the height of left subtree +
+        // the height of the right sub-tree + '1' for the current node
+        int diameter = leftTreeHeight + rightTreeHeight + 1;
+
+        // upfate the global tree diameter
+        treeDiameter = Math.max(treeDiameter, diameter);
+
+        // height of the current node will be equal to maximum of the height of
+        // left or right subtrees plus '1' for the current node
+        return Math.max(leftTreeHeight, rightTreeHeight) + 1;
+    }
 }
--- data-structure/src/test/java/ir/sk/algorithm/tree/RootToLeafPathTest.java
+++ data-structure/src/test/java/ir/sk/algorithm/tree/RootToLeafPathTest.java
@@ -131,4 +131,27 @@ public class RootToLeafPathTest {
         int result = RootToLeafPath.countAllPathSum(root, 11);
         System.out.println(result);
     }
+
+    @Test
+    public void findDiameter() {
+        TreeNode root = new TreeNode(1,
+                new TreeNode(2,
+                        new TreeNode(4),
+                        new TreeNode(5)
+                ),
+                new TreeNode(3,
+                        new TreeNode(5,
+                                new TreeNode(7),
+                                new TreeNode(8,
+                                        null,
+                                        new TreeNode(10))),
+                        new TreeNode(6,
+                                new TreeNode(9,
+                                        new TreeNode(11),
+                                        null),
+                                null)));
+
+        int result = RootToLeafPath.findDiameter(root);
+        System.out.println("Tree Diameter: " + result);
+    }
 }
