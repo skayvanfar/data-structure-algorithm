d8c657105f470c77435cf149420eb51769726f8d
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-02-21 15:32:55 +0330

    findMinCoinsByRecursive is added.

--- data-structure/src/main/java/ir/sk/algorithm/CoinExchangeProblem.java
+++ data-structure/src/main/java/ir/sk/algorithm/CoinExchangeProblem.java
@@ -6,7 +6,6 @@ import ir.sk.helper.technique.GreedyAlgorithm;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 
 /**
  * Created by sad.kayvanfar on 2/21/2021.
@@ -29,7 +28,7 @@ public class CoinExchangeProblem {
         List<Integer> result = new ArrayList<>();
 
         // Traverse through all
-        for (int i = coins.length - 1; i >= 0; i--) {
+        for (int i = 0; i< coins.length; i++) {
             // Find denominations
             while (sum >= coins[i]) {
                 sum -= coins[i];
@@ -37,5 +36,33 @@ public class CoinExchangeProblem {
             }
         }
         return result;
+    }
+
+    /**
+     * The time complexity of the solution is exponential
+     * @param coins
+     * @param sum
+     * @return
+     */
+    @TimeComplexity("O(2^n)")
+    public static int findMinCoinsByRecursive(int coins[], int sum) {
+        // base case
+        if (sum == 0) return 0;
+
+        // Initialize result
+        int res = Integer.MAX_VALUE;
+
+        // Try every coin that has smaller value than sum
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] <= sum) {
+                int sub_res = findMinCoinsByRecursive(coins, sum - coins[i]);
+
+                // Check for INT_MAX to avoid overflow and see if
+                // result can minimized
+                if (sub_res != Integer.MAX_VALUE && sub_res + 1 < res)
+                    res = sub_res + 1;
+            }
+        }
+        return res;
     }
 }
--- data-structure/src/test/java/ir/sk/algorithm/CoinExchangeProblemTest.java
+++ data-structure/src/test/java/ir/sk/algorithm/CoinExchangeProblemTest.java
@@ -19,12 +19,17 @@ public class CoinExchangeProblemTest {
 
     @Test
     public void findMinCoinsGreedyOptimal() {
-        System.out.println(CoinExchangeProblem.findMinCoinsGreedy(new int[]{1, 2, 5, 10, 20, 50, 100, 500, 1000}, 93));
+        System.out.println(CoinExchangeProblem.findMinCoinsGreedy(new int[]{1000, 500, 100, 50, 20, 10, 5, 2, 1}, 93));
     }
 
     @Test
     public void findMinCoinsUnOptimal() {
         // it doesn’t work for denominations {9, 6, 5, 1} and V = 11. The above approach would print 9, 1 and 1. But we can use 2 denominations 5 and 6.
-        System.out.println(CoinExchangeProblem.findMinCoinsGreedy(new int[]{1, 5, 6, 9}, 11));
+        System.out.println(CoinExchangeProblem.findMinCoinsGreedy(new int[]{9, 6, 5, 1}, 11));
+    }
+
+    @Test
+    public void findMinCoinsByRecursive() {
+        System.out.println(CoinExchangeProblem.findMinCoinsByRecursive(new int[]{9, 6, 5, 1}, 11));
     }
 }
