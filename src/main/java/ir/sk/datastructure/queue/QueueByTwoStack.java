ab67df0c283bb3e68ff3ef1eda1a904da57f2933
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-03-10 10:32:52 +0330

    fixes

--- src/main/java/ir/sk/datastructure/queue/QueueByTwoStack.java
+++ src/main/java/ir/sk/datastructure/queue/QueueByTwoStack.java
@@ -1,9 +1,24 @@
 package ir.sk.datastructure.queue;
 
+import ir.sk.helper.complexity.AmortizedTimeComplexity;
+import ir.sk.helper.complexity.TimeComplexity;
+
 import java.util.Stack;
 
 /**
  * A class which implements a queue using two stacks.
+ *
+ * Method 1 (By making enQueue operation costly) This method makes sure that oldest entered element is always at the top of stack 1,
+ * so that deQueue operation just pops from stack1.
+ * To put the element at top of stack1, stack2 is used.
+ *
+ * Method 2 (By making deQueue operation costly)In this method, in en-queue operation, the new element is entered at the top of stack1.
+ * In de-queue operation, if stack2 is empty then all the elements are moved to stack2 and finally top of stack2 is returned.
+ *
+ * Method 2 is definitely better than method 1.
+ * Method 1 moves all the elements twice in enQueue operation, while method 2 (in deQueue operation) moves the elements once and moves elements only if stack2 empty.
+ * So, the amortized complexity of the dequeue operation becomes O(1) .
+ *
  * <p>
  * Created by sad.kayvanfar on 10/4/2020.
  */
@@ -12,15 +27,16 @@ public class QueueByTwoStack<T> implements Queue<T> {
     private Stack<T> stack = new Stack<>();
     private Stack<T> buffer = new Stack<>();
 
-
+    @TimeComplexity("O(1)")
     public void enqueue(T value) {
         stack.push(value);
     }
 
+    @AmortizedTimeComplexity("O(1)")
+    @TimeComplexity("O(n)")
     public T dequeue() {
         shiftStacks(); // Ensure stackOldest has the current elements
         return buffer.pop(); // pop the oldest item.
-
     }
 
     public T peek() {
