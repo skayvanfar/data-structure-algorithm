5dee4e541376e3dbc4e0f803e7d3f9d286c3713f
Saeed Kayvanfar <skayvanfar.sj@gmail.com>
2021-04-19 17:56:55 +0430

    fixes

--- src/main/java/ir/sk/adt/datastructure/hashing/HashTable.java
+++ src/main/java/ir/sk/adt/datastructure/hashing/HashTable.java
@@ -2,6 +2,10 @@ package ir.sk.adt.datastructure.hashing;
 
 /**
  * A simple hashtable implementation
+ *
+ * A hash table is simply an array associated with a function (the hash function).
+ * It efficiently implements the dictionary ADT with efficient insert, remove and find operations, each taking O(1)O(1) expected time.
+ *
  */
 public class HashTable {
 
@@ -11,6 +15,12 @@ public class HashTable {
         this.array = new boolean[size];
     }
 
+    /**
+     * A hash function is used to determine the index where each key-value pair should go in the hash table
+     *
+     * @param key
+     * @return
+     */
     private int hash(int key) {
         return key % array.length;
     }
