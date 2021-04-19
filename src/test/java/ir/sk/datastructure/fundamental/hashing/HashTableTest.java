d8f6d8974159e3ef382adb8ead822a69415c63aa
Saeed Kayvanfar <skayvanfar.sj@gmail.com>
2021-04-19 17:33:34 +0430

    HashTable data structure is added.

---
+++ src/test/java/ir/sk/datastructure/fundamental/hashing/HashTableTest.java
@@ -0,0 +1,26 @@
+package ir.sk.datastructure.fundamental.hashing;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HashTableTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void contains() {
+    }
+}
