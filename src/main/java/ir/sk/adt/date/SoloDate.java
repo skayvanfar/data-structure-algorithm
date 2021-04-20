328c24a273d7ee94e06fc93a9f60cfd6b8d4e8f1
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-04-20 10:27:10 +0430

    soloDate is added.

---
+++ src/main/java/ir/sk/adt/date/SoloDate.java
@@ -0,0 +1,32 @@
+package ir.sk.adt.date;
+
+/**
+ * Created by sad.kayvanfar on 4/20/2021.
+ */
+public class SoloDate implements Date {
+
+    private final int value;
+
+    public SoloDate(int m, int d, int y) {
+        value = y * 512 + m * 32 + d;
+    }
+
+    @Override
+    public int month() {
+        return (value / 32) % 16;
+    }
+
+    @Override
+    public int day() {
+        return value % 32;
+    }
+
+    @Override
+    public int year() {
+        return value / 512;
+    }
+
+    public String toString() {
+        return month() + "/" + day() + "/" + year();
+    }
+}
