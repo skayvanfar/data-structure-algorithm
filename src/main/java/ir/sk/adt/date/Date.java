429870f97e3199d3b89be2842b2e62588f247d72
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-04-20 10:25:55 +0430

    partedDate is added.

---
+++ src/main/java/ir/sk/adt/date/Date.java
@@ -0,0 +1,10 @@
+package ir.sk.adt.date;
+
+/**
+ * Created by sad.kayvanfar on 4/20/2021.
+ */
+public interface Date {
+    int month();
+    int day();
+    int year();
+}
---
+++ src/main/java/ir/sk/adt/date/PartedDate.java
@@ -0,0 +1,37 @@
+package ir.sk.adt.date;
+
+/**
+ * Created by sad.kayvanfar on 4/20/2021.
+ */
+public class PartedDate implements Date {
+
+    private final int month;
+    private final int day;
+    private final int year;
+
+    public PartedDate(int m, int d, int y) {
+        month = m;
+        day = d;
+        year = y;
+    }
+
+    @Override
+    public int month() {
+        return month;
+    }
+
+    @Override
+    public int day() {
+        return day;
+    }
+
+    @Override
+    public int year() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return month() + "/" + day() + "/" + year();
+    }
+}
