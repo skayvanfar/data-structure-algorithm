0373174a0d00bb8186ca230c623473a3bc81ee0b
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-04-27 13:28:05 +0430

    fixes

--- src/main/java/ir/sk/adt/buffer/StackTextBuffer.java
+++ src/main/java/ir/sk/adt/buffer/StackTextBuffer.java
@@ -5,6 +5,8 @@ import ir.sk.helper.complexity.TimeComplexity;
 import java.util.Stack;
 
 /**
+ * a data type for a buffer in a text editor
+ *
  * Use two stacks: insertion gives O(1)
  *
  * Created by sad.kayvanfar on 3/9/2021.
@@ -19,17 +21,26 @@ public class StackTextBuffer implements TextBuffer {
         right = new Stack<>();
     }
 
+    /**
+     * insert c at the cursor position
+     */
     @TimeComplexity("O(1)")
     public void insert(char c) {
         right.push(c);
     }
 
+    /**
+     * delete and return the character at the cursor
+     */
     @TimeComplexity("O(1)")
     public char delete() {
         if (right.isEmpty()) return ' ';
         return right.pop();
     }
 
+    /**
+     * move the cursor k positions to the left
+     */
     @TimeComplexity("O(n)")
     public void left(int k) {
         for (int i = 0; i < k; i++) {
@@ -38,6 +49,9 @@ public class StackTextBuffer implements TextBuffer {
         }
     }
 
+    /**
+     * move the cursor k positions to the right
+     */
     @TimeComplexity("O(n)")
     public void right(int k) {
         for (int i = 0; i < k; i++) {
@@ -46,6 +60,9 @@ public class StackTextBuffer implements TextBuffer {
         }
     }
 
+    /**
+     * number of characters in the buffer
+     */
     public int size() {
         return left.size() + right.size();
     }
