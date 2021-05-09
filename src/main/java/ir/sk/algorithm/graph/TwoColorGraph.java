ee731686289926b0f9a2dddf6093d8bd54603cf3
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-05-09 13:56:42 +0430

    fixes

--- src/main/java/ir/sk/algorithm/graph/TwoColorGraph.java
+++ src/main/java/ir/sk/algorithm/graph/TwoColorGraph.java
@@ -4,6 +4,19 @@ package ir.sk.algorithm.graph;
  * Can the vertices of a given graph be assigned
  * one of two colors in such a way that no edge connects vertices of the same color ?
  * which is equivalent to this question: Is the graph bipartite ?
+ *
+ * bipartite graph: A bipartite graph is a graph whose vertices we can divide into two sets
+ * such that all edges connect a vertex in one set with a vertex in the other set.
+ * for example usage:
+ * file consists of lines listing a movie name followed
+ * by a list of the performers in the movie. In the
+ * context of graph processing, we can view it as defining
+ * a graph with movies and performers as vertices and
+ * each line defining the adjacency list of edges connecting
+ * each movie to its performers. Note that the graph
+ * is a bipartite graph—there are no edges connecting
+ * performers to performers or movies to movies.
+ *
  * <p>
  * Created by sad.kayvanfar on 5/9/2021.
  */
