a1895f04f91a0d5e53b9ce9d3b50781479a5cedb
Saeed Kayvanfar <skayvanfar.sj@gmail.com>
2021-05-14 22:01:21 +0430

    BellmanFordSP is added.

---
+++ src/main/java/ir/sk/algorithm/graph/Shortestpath/BellmanFordSP.java
@@ -0,0 +1,249 @@
+package ir.sk.algorithm.graph.Shortestpath;
+
+import ir.sk.adt.queue.LinkQueue;
+import ir.sk.adt.queue.Queue;
+import ir.sk.adt.stack.ResizingArrayStack;
+import ir.sk.adt.stack.Stack;
+import ir.sk.algorithm.graph.DirectedEdge;
+import ir.sk.algorithm.graph.EdgeWeightedDigraph;
+import ir.sk.algorithm.graph.EdgeWeightedDirectedCycle;
+
+/**
+ * Bellman-Ford shortest path algorithm. Computes the shortest path tree in
+ * edge-weighted digraph G from vertex s, or finds a negative cost cycle
+ * reachable from s.
+ * <p>
+ * The {@code BellmanFordSP} class represents a data type for solving the
+ * single-source shortest paths problem in edge-weighted digraphs with
+ * no negative cycles.
+ * The edge weights can be positive, negative, or zero.
+ * This class finds either a shortest path from the source vertex <em>s</em>
+ * to every other vertex or a negative cycle reachable from the source vertex.
+ * <p>
+ * This implementation uses a queue-based implementation of
+ * the Bellman-Ford-Moore algorithm.
+ * The constructor takes &Theta;(<em>E</em> <em>V</em>) time
+ * in the worst case, where <em>V</em> is the number of vertices and
+ * <em>E</em> is the number of edges. In practice, it performs much better.
+ * Each instance method takes &Theta;(1) time.
+ * It uses &Theta;(<em>V</em>) extra space (not including the
+ * edge-weighted digraph).
+ * <p>
+ * This correctly computes shortest paths if all arithmetic performed is
+ * without floating-point rounding error or arithmetic overflow.
+ * This is the case if all edge weights are integers and if none of the
+ * intermediate results exceeds 2<sup>52</sup>. Since all intermediate
+ * results are sums of edge weights, they are bounded by <em>V C</em>,
+ * where <em>V</em> is the number of vertices and <em>C</em> is the maximum
+ * absolute value of any edge weight.
+ */
+public class BellmanFordSP {
+
+    // for floating-point precision issues
+    private static final double EPSILON = 1E-14;
+
+    private double[] distTo;               // distTo[v] = distance  of shortest s->v path
+    private DirectedEdge[] edgeTo;         // edgeTo[v] = last edge on shortest s->v path
+    private boolean[] onQueue;             // onQueue[v] = is v currently on the queue?
+    private Queue<Integer> queue;          // queue of vertices to relax
+    private int cost;                      // number of calls to relax()
+    private Iterable<DirectedEdge> cycle;  // negative cycle (or null if no such cycle)
+
+    /**
+     * Computes a shortest paths tree from {@code s} to every other vertex in
+     * the edge-weighted digraph {@code G}.
+     * @param G the acyclic digraph
+     * @param s the source vertex
+     * @throws IllegalArgumentException unless {@code 0 <= s < V}
+     */
+    public BellmanFordSP(EdgeWeightedDigraph G, int s) {
+        distTo  = new double[G.vertexSize()];
+        edgeTo  = new DirectedEdge[G.vertexSize()];
+        onQueue = new boolean[G.vertexSize()];
+        for (int v = 0; v < G.vertexSize(); v++)
+            distTo[v] = Double.POSITIVE_INFINITY;
+        distTo[s] = 0.0;
+
+        // Bellman-Ford algorithm
+        queue = new LinkQueue<>();
+        queue.enqueue(s);
+        onQueue[s] = true;
+        while (!queue.isEmpty() && !hasNegativeCycle()) {
+            int v = queue.dequeue();
+            onQueue[v] = false;
+            relax(G, v);
+        }
+
+        assert check(G, s);
+    }
+
+    // relax vertex v and put other endpoints on queue if changed
+    private void relax(EdgeWeightedDigraph G, int v) {
+        for (DirectedEdge e : G.getNeighborsFor(v)) {
+            int w = e.to();
+            if (distTo[w] > distTo[v] + e.weight() + EPSILON) {
+                distTo[w] = distTo[v] + e.weight();
+                edgeTo[w] = e;
+                if (!onQueue[w]) {
+                    queue.enqueue(w);
+                    onQueue[w] = true;
+                }
+            }
+            if (++cost % G.vertexSize() == 0) {
+                findNegativeCycle();
+                if (hasNegativeCycle()) return;  // found a negative cycle
+            }
+        }
+    }
+
+    /**
+     * Is there a negative cycle reachable from the source vertex {@code s}?
+     * @return {@code true} if there is a negative cycle reachable from the
+     *    source vertex {@code s}, and {@code false} otherwise
+     */
+    public boolean hasNegativeCycle() {
+        return cycle != null;
+    }
+
+    /**
+     * Returns a negative cycle reachable from the source vertex {@code s}, or {@code null}
+     * if there is no such cycle.
+     * @return a negative cycle reachable from the soruce vertex {@code s}
+     *    as an iterable of edges, and {@code null} if there is no such cycle
+     */
+    public Iterable<DirectedEdge> negativeCycle() {
+        return cycle;
+    }
+
+    // by finding a cycle in predecessor graph
+    private void findNegativeCycle() {
+        int V = edgeTo.length;
+        EdgeWeightedDigraph spt = new EdgeWeightedDigraph(V);
+        for (int v = 0; v < V; v++)
+            if (edgeTo[v] != null)
+                spt.addEdge(edgeTo[v]);
+
+        EdgeWeightedDirectedCycle finder = new EdgeWeightedDirectedCycle(spt);
+        cycle = finder.cycle();
+    }
+
+    /**
+     * Returns the length of a shortest path from the source vertex {@code s} to vertex {@code v}.
+     * @param  v the destination vertex
+     * @return the length of a shortest path from the source vertex {@code s} to vertex {@code v};
+     *         {@code Double.POSITIVE_INFINITY} if no such path
+     * @throws UnsupportedOperationException if there is a negative cost cycle reachable
+     *         from the source vertex {@code s}
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public double distTo(int v) {
+        validateVertex(v);
+        if (hasNegativeCycle())
+            throw new UnsupportedOperationException("Negative cost cycle exists");
+        return distTo[v];
+    }
+
+    /**
+     * Is there a path from the source {@code s} to vertex {@code v}?
+     * @param  v the destination vertex
+     * @return {@code true} if there is a path from the source vertex
+     *         {@code s} to vertex {@code v}, and {@code false} otherwise
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public boolean hasPathTo(int v) {
+        validateVertex(v);
+        return distTo[v] < Double.POSITIVE_INFINITY;
+    }
+
+    /**
+     * Returns a shortest path from the source {@code s} to vertex {@code v}.
+     * @param  v the destination vertex
+     * @return a shortest path from the source {@code s} to vertex {@code v}
+     *         as an iterable of edges, and {@code null} if no such path
+     * @throws UnsupportedOperationException if there is a negative cost cycle reachable
+     *         from the source vertex {@code s}
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<DirectedEdge> pathTo(int v) {
+        validateVertex(v);
+        if (hasNegativeCycle())
+            throw new UnsupportedOperationException("Negative cost cycle exists");
+        if (!hasPathTo(v)) return null;
+        Stack<DirectedEdge> path = new ResizingArrayStack<>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+            path.push(e);
+        }
+        return path;
+    }
+
+    // check optimality conditions: either
+    // (i) there exists a negative cycle reacheable from s
+    //     or
+    // (ii)  for all edges e = v->w:            distTo[w] <= distTo[v] + e.weight()
+    // (ii') for all edges e = v->w on the SPT: distTo[w] == distTo[v] + e.weight()
+    private boolean check(EdgeWeightedDigraph G, int s) {
+
+        // has a negative cycle
+        if (hasNegativeCycle()) {
+            double weight = 0.0;
+            for (DirectedEdge e : negativeCycle()) {
+                weight += e.weight();
+            }
+            if (weight >= 0.0) {
+                System.err.println("error: weight of negative cycle = " + weight);
+                return false;
+            }
+        }
+
+        // no negative cycle reachable from source
+        else {
+
+            // check that distTo[v] and edgeTo[v] are consistent
+            if (distTo[s] != 0.0 || edgeTo[s] != null) {
+                System.err.println("distanceTo[s] and edgeTo[s] inconsistent");
+                return false;
+            }
+            for (int v = 0; v < G.vertexSize(); v++) {
+                if (v == s) continue;
+                if (edgeTo[v] == null && distTo[v] != Double.POSITIVE_INFINITY) {
+                    System.err.println("distTo[] and edgeTo[] inconsistent");
+                    return false;
+                }
+            }
+
+            // check that all edges e = v->w satisfy distTo[w] <= distTo[v] + e.weight()
+            for (int v = 0; v < G.vertexSize(); v++) {
+                for (DirectedEdge e : G.getNeighborsFor(v)) {
+                    int w = e.to();
+                    if (distTo[v] + e.weight() < distTo[w]) {
+                        System.err.println("edge " + e + " not relaxed");
+                        return false;
+                    }
+                }
+            }
+
+            // check that all edges e = v->w on SPT satisfy distTo[w] == distTo[v] + e.weight()
+            for (int w = 0; w < G.vertexSize(); w++) {
+                if (edgeTo[w] == null) continue;
+                DirectedEdge e = edgeTo[w];
+                int v = e.from();
+                if (w != e.to()) return false;
+                if (distTo[v] + e.weight() != distTo[w]) {
+                    System.err.println("edge " + e + " on shortest path not tight");
+                    return false;
+                }
+            }
+        }
+
+        System.out.println("Satisfies optimality conditions");
+        System.out.println();
+        return true;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        int V = distTo.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+}
---
+++ src/test/java/ir/sk/algorithm/graph/Shortestpath/BellmanFordSPTest.java
@@ -0,0 +1,53 @@
+package ir.sk.algorithm.graph.Shortestpath;
+
+import ir.sk.algorithm.graph.DirectedEdge;
+import ir.sk.algorithm.graph.EdgeWeightedDigraph;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BellmanFordSPTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void pathTo() {
+        EdgeWeightedDigraph graph = new EdgeWeightedDigraph(5);
+        graph.addEdge(new DirectedEdge(0, 1, 1));
+        graph.addEdge(new DirectedEdge(0, 2, 2));
+        graph.addEdge(new DirectedEdge(1, 3, 3));
+        graph.addEdge(new DirectedEdge(2, 3, 4));
+
+        BellmanFordSP sp = new BellmanFordSP(graph, 0);
+
+        // print negative cycle
+        if (sp.hasNegativeCycle()) {
+            for (DirectedEdge e : sp.negativeCycle())
+                System.out.println(e);
+        }
+
+        // print shortest paths
+        else {
+            for (int v = 0; v < graph.vertexSize(); v++) {
+                if (sp.hasPathTo(v)) {
+                    System.out.printf("%d to %d (%5.2f)  ", 0, v, sp.distTo(v));
+                    for (DirectedEdge e : sp.pathTo(v)) {
+                        System.out.print(e + "   ");
+                    }
+                    System.out.println();
+                }
+                else {
+                    System.out.printf("%d to %d           no path\n", 0, v);
+                }
+            }
+        }
+    }
+}
