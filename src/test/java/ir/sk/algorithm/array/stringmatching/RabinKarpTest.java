8d46a1f178a878a3637597c032fafce16a134e10
Saeed Kayvanfar <skayvanfar.sj@gmail.com>
2021-05-21 15:54:44 +0430

    RabinKarp is added.

---
+++ src/test/java/ir/sk/algorithm/array/stringmatching/RabinKarpTest.java
@@ -0,0 +1,36 @@
+package ir.sk.algorithm.array.stringmatching;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RabinKarpTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void search() {
+        String pat = "ABCDABD";
+        String txt = "ABC ABCDAB ABCDABCDABDE";
+
+        RabinKarp searcher = new RabinKarp(pat);
+        int offset = searcher.search(txt);
+
+        // print results
+        System.out.println("text:    " + txt);
+
+        // from brute force search method 1
+        System.out.print("pattern: ");
+        for (int i = 0; i < offset; i++)
+            System.out.print(" ");
+        System.out.println(pat);
+    }
+}
