05e1e2640778890a28f888adeab2b6f43a805d48
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-06-12 15:43:45 +0430

    ceilingInSortedArrayBinarySearch is added.

--- src/test/java/ir/sk/algorithm/tree/FloorCeilingTest.java
+++ src/test/java/ir/sk/algorithm/tree/FloorCeilingTest.java
@@ -30,4 +30,16 @@ public class FloorCeilingTest {
         else
             System.out.println("ceiling of "+x+" is "+arr[index]);
     }
+
+    @Test
+    public void ceilingInSortedArrayBinarySearch() {
+        int arr[] = {1, 2, 8, 10, 10, 12, 19};
+        int n = arr.length;
+        int x = 8;
+        int index = FloorCeiling.ceilingInSortedArrayBinarySearch(arr, 0, n-1, x);
+        if(index == -1)
+            System.out.println("Ceiling of "+x+" doesn't exist in array");
+        else
+            System.out.println("ceiling of "+x+" is "+arr[index]);
+    }
 }
