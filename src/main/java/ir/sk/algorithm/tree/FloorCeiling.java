d0e38cdfd87ed811c689b98c9746cd10f50be41a
Saeed Kayvanfar <skayvanfar.sj@gmail.com>
2021-07-05 19:35:52 +0430

    floorOfBST is added.

--- src/main/java/ir/sk/algorithm/tree/FloorCeiling.java
+++ src/main/java/ir/sk/algorithm/tree/FloorCeiling.java
@@ -99,15 +99,15 @@ public class FloorCeiling {
     /**
      * Imagine we are moving down the tree, and assume we are root node.
      * The comparison yields three possibilities,
-     *
+     * <p>
      * A) Root data is equal to key. We are done, root data is ceil value.
-     *
+     * <p>
      * B) Root data < key value, certainly the ceil value can't be in left subtree.
-     *    Proceed to search on right subtree as reduced problem instance.
-     *
+     * Proceed to search on right subtree as reduced problem instance.
+     * <p>
      * C) Root data > key value, the ceil value may be in left subtree.
-     *    We may find a node with is larger data than key value in left subtree,
-     *    if not the root itself will be ceil node.
+     * We may find a node with is larger data than key value in left subtree,
+     * if not the root itself will be ceil node.
      *
      * @param node
      * @param input
@@ -135,5 +135,42 @@ public class FloorCeiling {
         int ceil = ceilOfBST(node.left, input);
 
         return (ceil >= input) ? ceil : node.value;
+    }
+
+    /**
+     * 1 Start at the root Node.
+     * 2 If root->data == key,
+     *      floor of the key is equal
+     *      to the root.
+     * 3 Else if root->data > key, then
+     *      floor of the key must lie in the
+     *      left subtree.
+     * 4 Else floor may lie in the right subtree
+     *   but only if there is a value lesser than
+     *   or equal to the key.If not, then root is
+     *   the key.
+     *
+     * @param root
+     * @param key
+     * @return
+     */
+    @TimeComplexity("O(Log h)")
+    @SpaceComplexity("O(1)")
+    public static int floorOfBST(TreeNode root, int key) {
+        if (root == null)
+            return Integer.MAX_VALUE;
+
+        /* If root->data is equal to key */
+        if (root.value == key)
+            return root.value;
+
+        /* If root->data is greater than the key */
+        if (root.value > key)
+            return floorOfBST(root.left, key);
+
+        /* Else, the floor may lie in right subtree
+        or may be equal to the root*/
+        int floorValue = floorOfBST(root.right, key);
+        return (floorValue <= key) ? floorValue : root.value;
     }
 }
--- src/test/java/ir/sk/algorithm/tree/FloorCeilingTest.java
+++ src/test/java/ir/sk/algorithm/tree/FloorCeilingTest.java
@@ -56,4 +56,18 @@ public class FloorCeilingTest {
             System.out.println(i + " " + FloorCeiling.ceilOfBST(root, i));
         }
     }
+
+    @Test
+    public void floorOfBST() {
+        TreeNode root = new TreeNode(8);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(12);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(6);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(14);
+        for (int i = 0; i < 16; i++) {
+            System.out.println(i + " " + FloorCeiling.floorOfBST(root, i));
+        }
+    }
 }
