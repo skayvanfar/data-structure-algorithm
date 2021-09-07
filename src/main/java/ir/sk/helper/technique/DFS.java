28728b9d3ea7fa964740b89175943f64093c27c2
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-09-07 13:52:41 +0430

    fixes

--- src/main/java/ir/sk/helper/technique/DFS.java
+++ src/main/java/ir/sk/helper/technique/DFS.java
@@ -1,5 +1,12 @@
 package ir.sk.helper.technique;
 
+/**
+ *
+ * @see Backtracking is a more general purpose algorithm.
+ *
+ * Depth-First search is a specific form of backtracking related to searching tree structures.
+ * the difference between backtracking and DFS is that backtracking handles an implicit tree and DFS deals with an explicit one.
+ */
 @Backtracking
 public @interface DFS {
 }
