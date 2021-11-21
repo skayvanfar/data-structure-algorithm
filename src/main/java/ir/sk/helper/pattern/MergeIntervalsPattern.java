032d1d1ca42b282e1325bff1e9a9796dc6970bf6
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-11-21 14:09:16 +0330

    MergeIntervalsPattern is added.

---
+++ src/main/java/ir/sk/helper/pattern/MergeIntervalsPattern.java
@@ -0,0 +1,10 @@
+package ir.sk.helper.pattern;
+
+/**
+ * This technique is used to deal with overlapping intervals. Given two intervals (‘a’ and ‘b’),
+ * there will be six different ways the two intervals can relate to each other:
+ *
+ * Created by sad.kayvanfar on 11/21/2021.
+ */
+public @interface MergeIntervalsPattern {
+}
