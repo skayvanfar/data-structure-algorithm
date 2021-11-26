a10a0c1f664092af9da64566b926f892a1bf23ab
Saeed Kayvanfar <skayvanfar.sj@gmail.com>
2021-11-26 17:24:14 +0330

    fixes

--- src/main/java/ir/sk/algorithm/mathematic/Algorithms.java
+++ src/main/java/ir/sk/algorithm/mathematic/Algorithms.java
@@ -344,7 +344,8 @@ public class Algorithms {
         return array[r][c] + Math.min(findMinPath(array, r + 1, c), findMinPath(array, r, c + 1));
     }
 
-    @TimeComplexity("O(r+c)")
+    @TimeComplexity("O(r*c)")
+    @DynamicProgramming(type = DynamicProgrammingType.TOP_DAWN_MEMOIZATION)
     public static int findMinPathByDP(int[][] array, int r, int c, int[][] memo) {
         int R = array.length;
         int C = array[0].length;
