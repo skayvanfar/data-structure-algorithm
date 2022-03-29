e52ac309957003ae970640574093b8db38d7e5d9
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2022-03-30 00:42:16 +0200

    update comments

--- src/main/java/ir/sk/helper/paradigm/DivideAndConquer.java
+++ src/main/java/ir/sk/helper/paradigm/DivideAndConquer.java
@@ -1,6 +1,10 @@
 package ir.sk.helper.paradigm;
 
 /**
+ *  A divide-and-conquer algorithm recursively breaks down a problem into two or more sub-problems of the same or related type,
+ *  until these become simple enough to be solved directly.
+ *  The solutions to the sub-problems are then combined to give a solution to the original problem.
+ * 
  * Created by sad.kayvanfar on 9/6/2020.
  */
 public @interface DivideAndConquer {
