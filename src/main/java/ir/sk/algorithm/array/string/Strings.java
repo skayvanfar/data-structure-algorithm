0c5fd9a04326385f4099b384f1bf2339edb2675e
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2022-07-17 15:52:32 +0200

    add caesarCypherEncryptor

--- src/main/java/ir/sk/algorithm/array/string/Strings.java
+++ src/main/java/ir/sk/algorithm/array/string/Strings.java
@@ -224,4 +224,18 @@ public class Strings {
         }
         return result;
     }
+
+    public static String caesarCypherEncryptor(String str, int key) {
+        char[] newLetters = new char[str.length()];
+        int newKey = key % 26;
+        for (int i = 0; i < str.length(); i++) {
+          newLetters[i] = getNewLetter(str.charAt(i), newKey);
+        }
+        return new String(newLetters);
+      }
+    
+      private static char getNewLetter(char letter, int key) {
+        int newLetterCode = letter + key;
+        return newLetterCode <= 122 ? (char) newLetterCode : (char) (96 + newLetterCode % 122);
+      }
 }
