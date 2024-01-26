1174853fe73c0e5864b58e896f5a5e3f7cbad4a9
Saeid Keyvanfar <sad.keyvanfar@gmail.com>
2024-01-26 22:59:37 +0100

    add kthSmallestWithQuickSelect

--- src/main/java/ir/sk/algorithm/mathematic/SelectionProblem.java
+++ src/main/java/ir/sk/algorithm/mathematic/SelectionProblem.java
@@ -70,4 +70,67 @@ public class SelectionProblem {
         return median;
     }
 
+
+
+    // partition function similar to quick sort 
+    // Considers last element as pivot and adds 
+    // elements with less value to the left and 
+    // high value to the right and also changes 
+    // the pivot position to its respective position 
+    // in the final array. 
+    public static int partition(int[] arr, int low, 
+                                int high) 
+    { 
+        int pivot = arr[high], pivotloc = low; 
+        for (int i = low; i <= high; i++) { 
+            // inserting elements of less value 
+            // to the left of the pivot location 
+            if (arr[i] < pivot) { 
+                int temp = arr[i]; 
+                arr[i] = arr[pivotloc]; 
+                arr[pivotloc] = temp; 
+                pivotloc++; 
+            } 
+        } 
+  
+        // swapping pivot to the final pivot location 
+        int temp = arr[high]; 
+        arr[high] = arr[pivotloc]; 
+        arr[pivotloc] = temp; 
+  
+        return pivotloc; 
+    } 
+  
+    /**
+     * QuickSelect is a selection algorithm to find the k-th smallest element in an unordered list.
+     * It is related to the quick sort sorting algorithm.
+    */
+    // finds the kth position (of the sorted array) 
+    // in a given unsorted array i.e this function 
+    // can be used to find both kth largest and 
+    // kth smallest element in the array. 
+    // ASSUMPTION: all elements in arr[] are distinct 
+    @TimeComplexity("O(n)")
+    public static int kthSmallestWithQuickSelect(int[] arr, int low, 
+                                  int high, int k) 
+    { 
+        // find the partition 
+        int partition = partition(arr, low, high); 
+  
+        // if partition value is equal to the kth position, 
+        // return value at k. 
+        if (partition == k - 1) 
+            return arr[partition]; 
+  
+        // if partition value is less than kth position, 
+        // search right side of the array. 
+        else if (partition < k - 1) 
+            return kthSmallestWithQuickSelect(arr, partition + 1, high, k); 
+  
+        // if partition value is more than kth position, 
+        // search left side of the array. 
+        else
+            return kthSmallestWithQuickSelect(arr, low, partition - 1, k); 
+    } 
+  
 }
